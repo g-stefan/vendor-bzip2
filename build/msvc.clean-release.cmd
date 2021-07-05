74213982cf1f62dd093f6539e4578f40c1544122
Grigore Stefan <g_stefan@yahoo.com>
2021-07-05 14:36:09 +0300

    update

---
+++ build/msvc.clean-release.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> clean-release vendor-bzip2
+
+if exist release\ rmdir /Q /S release
---
+++ build/msvc.make.cmd
@@ -0,0 +1,30 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+set ACTION=%1
+if "%1" == "" set ACTION=make
+
+echo -^> %ACTION% vendor-bzip2
+
+goto StepX
+:cmdX
+%*
+if errorlevel 1 goto cmdXError
+goto :eof
+:cmdXError
+echo "Error: %ACTION%"
+exit 1
+:StepX
+
+call :cmdX xyo-cc --mode=%ACTION% --source-has-archive bzip2
+
+if not exist output\ mkdir output
+if not exist output\include\ mkdir output\include
+if not exist output\include\bzlib.h copy source\bzlib.h output\include\bzlib.h
+
+call :cmdX xyo-cc --mode=%ACTION% @build/source/libbz2.static.compile
+call :cmdX xyo-cc --mode=%ACTION% @build/source/libbz2.dynamic.compile
+call :cmdX xyo-cc --mode=%ACTION% @build/source/bzip2.compile
+call :cmdX xyo-cc --mode=%ACTION% @build/source/bzip2recover.compile
