116d2d640cbdd2163ed3a9957b2f0c3ffd369d5e
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:19:37 +0200

    Update

--- build/platform/msvc.clean.cmd
+++ build/platform/msvc.clean.cmd
@@ -3,4 +3,7 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> version-dependency vendor-bzip2
+echo - %BUILD_PROJECT% ^> clean
+
+if exist output\ rmdir /Q /S output
+if exist temp\ rmdir /Q /S temp
--- build/platform/msvc.vendor.cmd
+++ build/platform/msvc.vendor.cmd
@@ -3,6 +3,4 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> version-dependency-update vendor-bzip2
-
-xyo-cc --update-version-dependency vendor-bzip2
+echo - %BUILD_PROJECT% ^> vendor
